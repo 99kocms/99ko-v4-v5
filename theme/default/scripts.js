fc7479dec52b887e33f0e572dcb966cf0cbad759
MaxenceCauderlier <mx.koder@gmail.com>
2022-06-29 09:07:15 +0200

    [FIX] Anchors
    Fix pour les ancres : https://github.com/99kocms/99ko-v4-v5/issues/11
    Et ajout d'un décalage pour ne pas que les ancres se retrouvent sous le header

--- theme/default/scripts.js
+++ theme/default/scripts.js
@@ -1,10 +1,15 @@
-$(document).ready(function(){
-    $('#mobile_menu').click(function(){
-        if($('#navigation').css('display') == 'none'){
+$(document).ready(function () {
+    $('#mobile_menu').click(function () {
+        if ($('#navigation').css('display') == 'none') {
             $('#navigation').slideDown();
-        }
-        else{
+        } else {
             $('#navigation').slideUp();
         }
+    });
+    var pathname = window.location.href.split('#')[0];
+    $('a[href^="#"]').each(function () {
+        var $this = $(this),
+                link = $this.attr('href');
+        $this.attr('href', pathname + link);
     });
 });
